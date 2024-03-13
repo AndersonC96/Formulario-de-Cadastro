4694b60bcfdd1bc1c5e767ca2e12cc86b801b8b3
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-03-13 17:38:46 -0300

    Create form.php

---
+++ form.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Formulário de Cadastro</title>
+    </head>
+    <body>
+        <h2>Formulário de Cadastro</h2>
+        <!-- Formulário de cadastro -->
+    </body>
+</html>
