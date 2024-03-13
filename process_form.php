00cfef0e3a7a1bef4cc3e09fd3c56615bef18844
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-03-13 18:27:03 -0300

    Create process_form.php

---
+++ process_form.php
@@ -0,0 +1,24 @@
+<?php
+    include 'db.php';
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $nome = $_POST['nome'];
+        $telefone = $_POST['telefone'];
+        $profissao = $_POST['profissao'];
+        $numero_registro = $_POST['numero_registro'];
+        $cidade = $_POST['cidade'];
+        $estado = $_POST['estado'];
+        $data_hora = $_POST['data_hora'];
+        $representante = $_POST['representante'];
+        $sql = "INSERT INTO forms (nome, telefone, profissao, numero_registro, cidade, estado, data_hora, representante) VALUES ('$nome', '$telefone', '$profissao', '$numero_registro', '$cidade', '$estado', '$data_hora', '$representante')";
+        if(mysqli_query($conn, $sql)){
+            header("Location: form.php?success=true");
+            exit();
+        }else{
+            header("Location: form.php?success=false&error=" . urlencode(mysqli_error($conn)));
+            exit();
+        }
+    }else{
+        header("Location: form.php");
+        exit();
+    }
+?>
