e9ff6ff6d0d87e3cbaaf27f9d82326d1ac826712
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-03-19 17:49:39 -0300

    Update success.php

--- success.php
+++ success.php
@@ -1,20 +1,29 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Sucesso!</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    </head>
-    <body>
-        <div class="container mt-4">
-            <div class="row justify-content-center">
-                <div class="col-md-6">
-                    <div class="alert alert-success" role="alert">Usuário criado com sucesso!</div>
-                    <a href="dashboard.php" class="btn btn-primary">Voltar ao Painel de Controle</a>
-                </div>
-            </div>
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-    </body>
-</html>
+<?php
+    session_start();
+    if(!isset($_SESSION['user_id'])){
+        header("Location: index.php");
+        exit();
+    }
+    include 'db.php';
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $nome = $_POST['nome'];
+        $sobrenome = $_POST['sobrenome'];
+        $username = $_POST['username'];
+        $password = md5($_POST['password']);
+        $admin = $_POST['is_admin'];
+        $sql = "INSERT INTO users (nome, sobrenome, username, password, is_admin) VALUES (?, ?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        if($stmt){
+            $stmt->bind_param("ssssi", $nome, $sobrenome, $username, $password, $admin);
+            $stmt->execute();
+            if($stmt->affected_rows > 0){
+                header("Location: dashboard.php?success=true");
+                exit();
+            }else{
+                echo "Erro ao adicionar o usuário: " . $conn->error;
+            }
+        }else{
+            echo "Erro ao preparar a consulta: " . $conn->error;
+        }
+    }
+?>
