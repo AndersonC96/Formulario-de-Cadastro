393564ead5d6b2a68530356062b35a7663dbc5e0
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-03-19 11:41:46 -0300

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -3,7 +3,7 @@
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         $nome = $_POST['nome'];
         $telefone = $_POST['telefone'];
-        $celuar = $_POST['celular'];
+        $celular = $_POST['celular'];
         $email = $_POST['email'];
         $profissao = $_POST['profissao'];
         $numero_registro = $_POST['numero_registro'];
@@ -11,16 +11,15 @@
         $estado = $_POST['estado'];
         $data_hora = $_POST['data_hora'];
         $representante = $_POST['representante'];
-        $sql = "INSERT INTO forms (nome, telefone, celular, email, profissao, numero_registro, cidade, estado, data_hora, representante) VALUES ('$nome', '$telefone', '$celular', '$email', '$profissao', '$numero_registro', '$cidade', '$estado', '$data_hora', '$representante')";
-        if(mysqli_query($conn, $sql)){
+        $sql = "INSERT INTO forms (nome, telefone, celular, email, profissao, numero_registro, cidade, estado, data_hora, representante) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ssssssssss", $nome, $telefone, $celular, $email, $profissao, $numero_registro, $cidade, $estado, $data_hora, $representante);
+        if($stmt->execute()){
             header("Location: form.php?success=true");
             exit();
         }else{
-            header("Location: form.php?success=false&error=" . urlencode(mysqli_error($conn)));
+            header("Location: form.php?success=false&error=" . urlencode($stmt->error));
             exit();
         }
-    }else{
-        header("Location: form.php");
-        exit();
     }
 ?>
