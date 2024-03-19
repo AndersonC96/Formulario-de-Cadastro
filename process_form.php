07adfd8f75da8df3328fa1beeb21529b9e021e18
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-03-19 11:22:23 -0300

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -3,13 +3,15 @@
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         $nome = $_POST['nome'];
         $telefone = $_POST['telefone'];
+        $celuar = $_POST['celular'];
+        $email = $_POST['email'];
         $profissao = $_POST['profissao'];
         $numero_registro = $_POST['numero_registro'];
         $cidade = $_POST['cidade'];
         $estado = $_POST['estado'];
         $data_hora = $_POST['data_hora'];
         $representante = $_POST['representante'];
-        $sql = "INSERT INTO forms (nome, telefone, profissao, numero_registro, cidade, estado, data_hora, representante) VALUES ('$nome', '$telefone', '$profissao', '$numero_registro', '$cidade', '$estado', '$data_hora', '$representante')";
+        $sql = "INSERT INTO forms (nome, telefone, celular, email, profissao, numero_registro, cidade, estado, data_hora, representante) VALUES ('$nome', '$telefone', '$celular', '$email', '$profissao', '$numero_registro', '$cidade', '$estado', '$data_hora', '$representante')";
         if(mysqli_query($conn, $sql)){
             header("Location: form.php?success=true");
             exit();
