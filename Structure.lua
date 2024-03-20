6659ff9cc42945bd50108ccc53c6ac13738e34fe
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-03-20 16:46:29 -0300

    Update Structure.lua

--- Structure.lua
+++ Structure.lua
@@ -1,11 +1,26 @@
 projeto/
 │
-├── db.php
-├── index.php
-├── dashboard.php
+├── CSS/
+│   ├── navbar.css
+│   ├── style.css
+├── Database/
+│   ├── db.sql
+├── JS/
+├── Vendor/
 ├── create_user.php
-├── success.php
-├── form.php
+├── dashboard.php
+├── db.php
+├── edit_form.php
+├── edit_user.php
 ├── export.php
+├── form.php
+├── index.php
+├── logout.php
 ├── process_form.php
-└── logout.php
+├── remove_form.php
+├── remove_user.php
+├── success.php
+├── update_form.php
+├── update_user.php
+├── view_forms.php
+└── view_users.php
