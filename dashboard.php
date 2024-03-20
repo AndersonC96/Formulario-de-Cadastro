031679a9b87c5d977fb8b949d17854b4c0877811
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-03-20 14:29:22 -0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -46,7 +46,7 @@
                             <a class="nav-link" href="form.php"><i class="fas fa-edit"></i> Formulário</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="export.php"><i class="fas fa-eye"></i> Ver Formulários</a>
+                            <a class="nav-link" href="view_forms.php"><i class="fas fa-eye"></i> Ver Formulários</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="export.php"><i class="fas fa-file-export"></i> Exportar</a>
