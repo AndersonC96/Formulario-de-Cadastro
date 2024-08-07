5fda8b60eb8c58b136a234a6a7ff02dbac6a3a54
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-08-07 11:35:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Simple Pharma - Visitação | Login</title>
+        <title>Simple Pharma - Formulários | Login</title>
         <link rel="icon" href="https://static.wixstatic.com/media/5ede7b_719545c97a084f288b8566db52756425%7Emv2.png/v1/fill/w_180%2Ch_180%2Clg_1%2Cusm_0.66_1.00_0.01/5ede7b_719545c97a084f288b8566db52756425%7Emv2.png" type="image/x-icon" />
         <link rel="stylesheet" href="./CSS/style.css">
     </head>
